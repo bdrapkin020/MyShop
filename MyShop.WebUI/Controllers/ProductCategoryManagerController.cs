9116f6774dfcd38351f6d597e53d19b56b89df21
bdrapkin020 <61131409+bdrapkin020@users.noreply.github.com>
2021-03-12 21:11:51 -0700

    Added DI

--- MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
+++ MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
 using System;
 using System.Collections.Generic;
@@ -17,11 +18,16 @@ namespace MyShop.WebUI.Controllers
         //    context = new ProductCategoryRepository();
         //}
 
-        InMemoryRepository<ProductCategory> context;
+        IRepository<ProductCategory> context;
 
-        public ProductCategoryManagerController()
+        //public ProductCategoryManagerController()
+        //{
+        //    context = new InMemoryRepository<ProductCategory>();
+        //}
+
+        public ProductCategoryManagerController(IRepository<ProductCategory> context)
         {
-            context = new InMemoryRepository<ProductCategory>();
+            this.context = context;
         }
 
         // GET: ProductManager
